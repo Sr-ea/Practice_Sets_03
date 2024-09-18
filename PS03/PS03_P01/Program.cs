bd4ea6e9073481fcf0a2f2a5d85b92dc2b0752ac
Rhea Lee Salonoy <svynry@gmail.com>
2024-09-18 21:32:09 +0800

    Added My Program

---
+++ PS03/PS03_P01/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+    namespace PS03_P01
+    {
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+            //Create a function that computes the area of a triangle using Heron’s formula
+            Console.Write("Enter length of side 1:");
+            double a = double.Parse(Console.ReadLine());
+
+            Console.Write("Enter length of side 2:");
+            double b = double.Parse(Console.ReadLine());
+
+            Console.Write("Enter length of side 3:");
+            double c = double.Parse(Console.ReadLine());
+
+            double area = ComputeArea(a, b, c);
+
+            Console.WriteLine($"The area of the triangle is: {area}");
+             
+            }
+
+            static double ComputeArea(double a, double b, double c)
+            {
+                double s = (a + b + c) / 2;
+                double area = Math.Sqrt(s * (s - a) * (s - b) * (s - c));
+
+                return area;
+            }
+        }
+    }
+ 
---
+++ PS03/PS03_P02/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace PS03_P02
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Create a function that determines whether a given year is a leap year or not.
+            Console.Write("Enter the year here:");
+            int yr = int.Parse(Console.ReadLine());
+
+            bool year = leapyearchecker(yr);
+            if (!year)
+            {
+                Console.WriteLine($"The year {yr} is not a leap year");
+            }
+
+            else
+            {
+                Console.WriteLine($"The year {yr} is a leap year");
+            }
+        }
+
+        public static bool leapyearchecker(int yr)
+        {
+           return (yr % 4 ==0 && yr % 100 != 0) || yr % 400 == 0;
+        }
+    }
+}
